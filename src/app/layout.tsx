6f9ef8acfc1163389f9c749cd3e3583a3d551337
Jpc <Joaop3595@gmail.com>
2025-04-17 03:41:57 -0300

    feat: add navbar desktop and mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,7 +31,18 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+          <div className="min-h-screen">
+            <Navbar />
+            <main className="py-8">
+              <div className="max-w-7xl mx-auto">
+                <div className="grid grid-cols-1 lg:grid-cols-12 gap-6"> 
+                  <div className="lg:col-span-12">
+                    {children}
+                  </div>
+                </div>
+              </div>
+            </main>
+          </div>
         </body>
       </html>
     </ClerkProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <SignedOut >
-        <SignInButton mode="modal" />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
     </div>
   );
 }
