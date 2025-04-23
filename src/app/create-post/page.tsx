13cca734db220f6a19d3cce88eef50fa8e9779a1
Jpc <Joaop3595@gmail.com>
2025-04-22 23:46:45 -0300

    feat: add 4-step form to create post

---
+++ src/app/create-post/page.tsx
@@ -0,0 +1,176 @@
+"use client"
+
+import Map from '@/components/Map'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+import React, { useState } from 'react'
+
+function CreatePost() {
+
+  const [step, setStep] = useState(1);
+
+  const next = () => setStep((prev) => prev + 1);
+  const back = () => setStep((prev) => prev - 1);
+
+  const [postForm, setPostForm] = useState({
+    petName: "",
+    species: "",
+    breed: "",
+    color: "",
+    gender: "",
+    ageApprox: "",
+    description: "",
+    missingDate: "",
+    photos: [],
+  });
+
+  return (
+    <Card >
+      <CardHeader>
+        <CardTitle className='text-lg flex justify-between'>
+          Report Pet
+          <div className='font-light text-sm flex justify-end'>
+            Step {step} of 4
+          </div>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-4">
+          {step === 1 && (
+
+          <>
+            <div className="space-y-2">
+              <Label>Pet name</Label>
+              <Input
+                name="pet-name"
+                value={postForm.petName}
+                onChange={(e) => setPostForm({ ...postForm, petName: e.target.value })}
+                placeholder="Lost/Found pet name"
+              />
+            </div>
+            <div className="space-y-2">
+              <Label>Species</Label>
+              <Input
+                name="species"
+                value={postForm.species}
+                onChange={(e) => setPostForm({ ...postForm, species: e.target.value })}
+                placeholder="Pet Species"
+              />
+            </div>
+            <div className="space-y-2">
+              <Label>Breed</Label>
+              <Input
+                name="breed"
+                value={postForm.breed}
+                onChange={(e) => setPostForm({ ...postForm, breed: e.target.value })}
+                placeholder="Pet breed"
+              />
+            </div>
+            <div className="space-y-2">
+              <Label>Color</Label>
+              <Input
+                name="color"
+                value={postForm.color}
+                onChange={(e) => setPostForm({ ...postForm, color: e.target.value })}
+                placeholder="Pet color"
+              />
+            </div>
+            <div className="space-y-2">
+              <Label>Age</Label>
+              <Input
+                name="age"
+                value={postForm.ageApprox}
+                onChange={(e) => setPostForm({ ...postForm, ageApprox: e.target.value })}
+                placeholder="Approximate age of the pet"
+              />
+            </div>
+            <div className="space-y-2">
+              <Label>Gender</Label>
+              <Input
+                name="gender"
+                value={postForm.gender}
+                onChange={(e) => setPostForm({ ...postForm, gender: e.target.value })}
+                placeholder="Pet gender"
+              />
+            </div>
+          </>
+          )}
+
+          {step === 2 && (
+            <>  
+              <div className="space-y-2">
+                <Label>Missing Date</Label>
+                <Input
+                  name="date"
+                  value={postForm.missingDate}
+                  onChange={(e) => setPostForm({ ...postForm, missingDate: e.target.value })}
+                  placeholder="When did you lose the pet?"
+                />
+              </div>
+              <div className="space-y-2">
+                <Label>Description</Label>
+                <Textarea
+                  name="description"
+                  value={postForm.description}
+                  onChange={(e) => setPostForm({ ...postForm, description: e.target.value })}
+                  className="min-h-[100px]"
+                  placeholder="Any information about the pet"
+                />
+              </div>
+            </>
+            
+          )}
+
+          {step === 3 && (
+            <>
+              <div className="space-y-2">
+                <Label>Photos</Label>
+                <Input
+                  name="photos"
+                  type="file"
+                  accept="image/*"
+                  multiple
+                  // onChange={ TODO }
+                />
+              </div>
+            </>
+          )}
+
+          {step === 4 && (
+            <>
+              <div className="space-y-2">
+                <Label>Location</Label>
+                <Map />
+              </div>
+            </>
+          )}
+
+        </div>
+      </CardContent>
+      <CardFooter className='flex justify-between' >
+        {step > 1 && (
+          <Button className='w-32' variant="outline" onClick={back} >
+            Back
+          </Button>
+        )}
+        {step < 4 && (
+          <div className='flex justify-end w-full'>
+            <Button className='w-32' variant="outline" onClick={next} >
+              Next
+            </Button>
+          </div>
+        )}
+        {step === 4 && (
+          <Button className='w-32' variant="outline" type="submit">
+            Post
+          </Button>
+        )}
+      </CardFooter>
+    </Card>
+  )
+}
+
+export default CreatePost
