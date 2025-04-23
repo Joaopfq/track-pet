65d5caf6650cbcedb626fa34351e3bbf926a056f
Jpc <Joaop3595@gmail.com>
2025-04-22 23:42:49 -0300

    feat: add create post button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      
+      <Link href="/create-post">
+        <Button className=" bg-muted hover:bg-muted/80 size-14 fixed bottom-6 right-6 rounded-full shadow-lg w-2xl text-3xl">
+          +
+        </Button>
+      </Link>
     </div>
   );
 }
