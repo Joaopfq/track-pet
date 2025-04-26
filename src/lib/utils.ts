2aa9a60a7d7193f920139209ebf6a86677137739
Jpc <Joaop3595@gmail.com>
2025-04-26 02:24:03 -0300

    feat: add convert enum to string function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function mapEnumToString<T extends Record<string, string>>(enumValue: T[keyof T]): string {
+  return enumValue as string;
+}
+
+export function mapStringToEnum<T extends Record<string, string>>(enumObj: T, value: string): T[keyof T] | undefined {
+  return Object.values(enumObj).includes(value) ? (value as T[keyof T]) : undefined;
+}
