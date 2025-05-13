e355e82223b18706d2de295da70f055e3b4c200f
Jpc <Joaop3595@gmail.com>
2025-05-12 23:37:03 -0300

    feat: add profile page

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,9 +1,36 @@
+import { getProfileByUsername, getUserPosts} from '@/actions/profile'
+import { notFound } from 'next/navigation';
 import React from 'react'
+import ProfilePageClient from './ProfilePage';
 
-function ProfilePage() {
+
+export async function generateMetadata({params}: {params: {username: string}}) {
+  
+  const user = await getProfileByUsername(params.username);
+
+  if (!user) return null;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: `Welcome to ${user.name ?? user.username}'s profile`,
+  };
+}
+
+
+async function ProfilePageServer({params}: {params: {username: string}}) {
+
+  const user = await getProfileByUsername(params.username);
+  if (!user) notFound();
+
+  const post = await getUserPosts(user.id);
+
+
   return (
-    <div>ProfilePage</div>
+    <ProfilePageClient 
+    user={user}
+    posts={post}
+    />
   )
 }
 
-export default ProfilePage
+export default ProfilePageServer
