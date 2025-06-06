a94fb7554f4d16f43add037d3ac7e66466fc0a9e
Jpc <Joaop3595@gmail.com>
2025-06-06 02:30:12 -0300

    refactor: change leaflet css to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       <html lang="en">
         <head>
           <link 
-            rel="preconnect"
+            rel="stylesheet"
             href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
             integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
             crossOrigin=""/>
