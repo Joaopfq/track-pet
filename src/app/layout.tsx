d87ef1993ab0ddb8cd868ba031b025b8ae56c7aa
Jpc <Joaop3595@gmail.com>
2025-06-10 20:54:39 -0300

    perf: import leaflet css via NPM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { ThemeProvider } from "next-themes";
 import Sidebar from "@/components/Sidebar";
 import { Toaster } from "react-hot-toast";
 import StoreProvider from '../app/StoreProvider'
+import 'leaflet/dist/leaflet.css';
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -34,13 +35,6 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <head>
-          <link 
-            rel="stylesheet"
-            href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-            integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-            crossOrigin=""/>
-        </head>
         <body
           className={`${poppins.variable} ${roboto.variable} antialiased`}
         >
